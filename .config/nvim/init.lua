ede8eab0e95c774c333d53019045df9092aa2217
Mark Morley <wintermute@pop-os.localdomain>
2023-01-26 20:38:04 -0500

    added nvim & alacritty dots

---
+++ .config/nvim/init.lua
@@ -0,0 +1,6 @@
+require('plugins')
+require('settings')
+require('nvimtree')
+require('colortheme')
+require('comment')
+require('statusline')
---
+++ .config/nvim/lua/plugins.lua
@@ -0,0 +1,37 @@
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function()
+	use 'wbthomason/packer.nvim'
+
+	use {
+	  'nvim-tree/nvim-tree.lua',
+  	requires = {
+    	'nvim-tree/nvim-web-devicons', -- optional, for file icons
+  		},
+	}
+	use 'tpope/vim-fugitive'
+
+	use 'lewis6991/gitsigns.nvim'
+
+  use {
+  'nvim-lualine/lualine.nvim',
+  requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+    }
+
+	use 'lukas-reineke/indent-blankline.nvim' -- Add indentation guides even on blank lines
+
+	use { -- LSP Configuration & Plugins
+    'neovim/nvim-lspconfig',
+  requires = {
+      -- Automatically install LSPs to stdpath for neovim
+    'williamboman/mason.nvim',
+    'williamboman/mason-lspconfig.nvim',
+
+      -- Useful status updates for LSP
+    'j-hui/fidget.nvim',
+   },
+  }	
+  	use { "ellisonleao/gruvbox.nvim" }
+
+    use {"terrortylor/nvim-comment"}
+end)
